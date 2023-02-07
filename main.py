7e60f57f087d67e8732562be9eb45eac94f176e7
Aluno da Infinity School <ruancom01@gmail.com>
2023-02-07 20:21:57 -0300

    Atualização do cod

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def CalcularIMC(peso,altura):
-    return peso/altura
+    return peso/(altura **2)
 peso = float(input('Digite seu peso:'))
 altura = float(input('Digite altura:'))
 print(CalcularIMC(peso,altura))
